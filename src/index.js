6f121ed524ebc6628fa990ae656e7759c8e08da4
John Haugeland <stonecypher@gmail.com>
2016-03-11 14:02:59 -0800

    control of port, silent mode, unused stub for mime types, better help, better structure

--- src/index.js
+++ src/index.js
@@ -21,8 +21,10 @@ if (cli.help) {
     res.send('Hello World!');
   });
 
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  app.listen(cli.port, function () {
+    if (!(cli.silent)) {
+      console.log('Example app listening on port ' + cli.port.toString() + '!');
+    }
   });
 
 }
