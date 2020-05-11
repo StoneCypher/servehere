830805289809808d4471024db377b166fe61b43d
John Haugeland <stonecypher@gmail.com>
2020-05-10 22:25:06 -0700

    add -r and -R for default root object behavior

--- src/arghelp.js
+++ src/arghelp.js
@@ -6,13 +6,15 @@ const args = [
   { name: "directory", alias: "d", type: String,  typelabel: '{underline file}', defaultValue: './',                       description: "Choose which directory to serve from, if not here" },
   { name: "cors",      alias: "c",                                                                                         description: "Emit open-ended CORS headers, often to support mock API development" },
   { name: "jsonapi",   alias: "j", type: Boolean,                                defaultValue: false,                      description: "Serve no-extension files as application/json, to mimic a JSON api" },
-  { name: "silent",    alias: "s", type: Boolean,                                defaultValue: false,                      description: "Whether to run without output" }
+  { name: "silent",    alias: "s", type: Boolean,                                defaultValue: false,                      description: "Whether to run without output" },
+  { name: "dro",       alias: "r", type: String,  typelabel: '{underline file}', defaultValue: false,                      description: "Filename of default root object, typically for SPAs" },
+  { name: "drosc",     alias: "R", type: Number,                                 defaultValue: false,                      description: "HTTP status code of default root object, typically for SPAs" }
 
 ];
 
 const sections = [
 
-  { header: 'ServeHere', content: '   ServeHere is a trivial webserver, mostly meant for developers, that stands up a server where you are at the time - no hassle, no fuss.  ServeHere is great for verifying build output, automated tests, and checking things without CORS or localhost problems.' },
+  { header: 'ServeHere', content: '   {bold ServeHere} is a trivial webserver, mostly meant for developers, that stands up a server where you are at the time - no hassle, no fuss.  {bold ServeHere} is great for verifying build output, automated tests, and checking things without CORS or localhost problems.  {bold ServeHere} is especially useful in {bold package.json} scripts.' },
   { header: 'Options',   optionList: args },
 
   { header: 'Examples',  content: [
@@ -29,7 +31,20 @@ const sections = [
     { desc: '{underline Silent}, for when scripts need this server to {bold shut up}',
       example: '$ servehere --silent' },
     { desc: '{underline Testing structure} - serve {bold ./fixtures} as {bold json} on {bold 80} with {bold cors}',
-      example: '$ servehere -d fixtures/ -j -p 80 -c'}
+      example: '$ servehere -d fixtures/ -j -p 80 -c'},
+    { desc: '{underline Default root object} - serve {bold ./foo/bar.html} for any missing call',
+      example: '$ servehere -r docs/index.html'},
+    { desc: '{underline Default root object} - serve {bold ./foo/bar.html} for any missing call with HTTP 200',
+      example: '$ servehere -r docs/index.html -R 200'}
+  ] },
+
+  { header: 'Installation', content: [
+    { desc: '{underline Global}',
+      example: 'npm install -g servehere' },
+    { desc: '{underline In-project for devs}',
+      example: 'npm install --save-dev servehere'},
+    { desc: '{underline In-project for users}',
+      example: 'npm install --save servehere'}
   ] },
 
   { header: 'Reference', content: 'Find reference and a bug tracker at the {bold ServeHere} webpage, {underline {cyan https://github.com/StoneCypher/servehere/}}' }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const argh    = require('./arghelp.js'),
 
 const page404 = '<!doctype html><html><head><title>ServeHere 404 page</title><style type="text/css">html,body,h1{margin:0;padding:0;border:0;}p{padding:2em;line-height:165%;text-indent:2.25em;}body{font-size:300%;color:white;background-color:#0A448F;}h1{font-size:200%;padding:1em;background-color:#28244C;border-bottom:1px solid #191730;}#bug{position:fixed;bottom:2em;right:2em;background-color:#28244C;padding:1em;border-radius:0.33em;border: 2px solid #191730;}#bug a{color:#def;}</style></head><body><h1>404 - Not Found</h1><p>Sorry, but, the requested page or endpoint does either not exist, or this server does not have privileges to offer it.  As a result, this request is denied.  Please check the resource you are requesting and the credentials you are presenting, and try your request again.  No further information is available.</p><div id="bug"><a rel="noopener noreferrer" target="_blank" href="https://github.com/StoneCypher/servehere/">Hosted by ServeHere</a></div></body></html>';
 
+let dro   = undefined,
+    drosc = false;
+
 
 
 
@@ -36,7 +39,13 @@ if (cli.help !== undefined) {  // because it's null in 5, sigh
 
   app.use(express.static(cli.directory));
 
-  app.use( (_req, res, _next) => res.status(404).send(page404) );
+  if (cli.dro) {
+    dro = `${require('fs').readFileSync(cli.dro)}`;
+  }
+
+  drosc = cli.drosc;
+
+  app.use( (_req, res, _next) => res.status(dro? (drosc || 404) : 404).send(dro || page404) );
 
   app.listen(cli.port, function () {
     if (!(cli.silent)) {
